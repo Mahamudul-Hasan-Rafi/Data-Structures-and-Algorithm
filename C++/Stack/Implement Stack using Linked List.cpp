bba238ec14dcf3ae5864a53c7c97b4e131b19c6d
Mahamudul Hasan Rafi <46628039+Mahamudul-Hasan-Rafi@users.noreply.github.com>
2023-01-12 01:42:27 +0600

    Update Implement Stack using Linked List.cpp

--- C++/Stack/Implement Stack using Linked List.cpp
+++ C++/Stack/Implement Stack using Linked List.cpp
@@ -1,21 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+struct StackNode{
+    int data;
+    StackNode* next;
 
-//Function to push an integer into the stack.
-void MyStack ::push(int x) 
-{
+    StackNode(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+class MyStack{
+private:
+    StackNode* top;
+
+public:
+    MyStack(){top=NULL;}
+    void push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+void MyStack::push(int x){
     StackNode* node = new StackNode(x);
-    
+
     if(top!=NULL)
         node->next=top;
     top=node;
 }
 
-//Function to remove an item from top of the stack.
-int MyStack ::pop() 
-{
+int MyStack::pop(){
     if(top==NULL)
         return -1;
-    int data=top->data;
+    int temp=top->data;
     top=top->next;
-    return data;
+
+    return temp;
+}
+
+bool MyStack::isEmpty(){
+    if(top==NULL)
+        return true;
+    return false;
+}
+
+int MyStack::peek(){
+    if(top==NULL)
+        return -1;
+    return top->data;
+}
+
+int main()
+{
+    MyStack stck;
+
+    stck.push(77);
+    stck.push(96);
+    stck.push(3);
+    stck.push(55);
+    stck.push(100);
+
+    cout<<stck.pop()<<" popped"<<endl;
+
+    cout<<stck.peek()<<" top element"<<endl;
+
+    while(!stck.isEmpty()){
+        cout<<stck.peek()<<" ";
+        stck.pop();
+    }
+
+    return 0;
 }
